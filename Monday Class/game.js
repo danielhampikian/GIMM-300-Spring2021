0eaf46e0c985365d1a4dc0d17a633d39a79af8a4
Daniel Hampikian <danielhampikian@gmail.com>
2021-03-22 20:08:20 -0600

    basic core done

--- Monday Class/game.js
+++ Monday Class/game.js
@@ -16,6 +16,12 @@ var cardsBankLength = 100;
 var playerCards = [];
 var enemyCards = [];
 
+var cardsDamage; 
+var cardsDefense;
+var cardsName;
+var cards;
+var cardImage;
+
 
 
 //display variables
@@ -27,23 +33,25 @@ var enemyHealthDisp = document.getElementById('enemyHealth');
 const playerTurnButton = document.getElementById('playerTurn');
 playerTurnButton.addEventListener('click', playerAttack);
 
-var enemyTurnButton = document.getElementById('playerTurn');
-enemyTurnButton.onclick = function() {
-    enemyTurn()
-};
+const doneButton = document.getElementById('done');
+doneButton.addEventListener('click', done);
+
 const upgradeButton = document.getElementById('play');
 upgradeButton.addEventListener('click', startGame);
 
 
 function playerAttack() {
-
+    if(!gameOver()) {
+    console.log("In player attack");
+    cardBattle();
+    gameOver();
+    }
 }
 
 
 function playerTurn(){
     if(!gameOver()) {
-    enemyHealth = enemyHealth - 10;
-    updateDisp();
+    
     gameOver();
     }
 }
@@ -57,11 +65,12 @@ function setUpCardBanks() {
         cardStats.push(getRandomColor());
         // [1, 2, 'random name', 'random color']
         cardStats.push(i);
-        // [1, 2, 'random name', 'random color', 'enemy_id_3' 'image url']
+        // [1, 2, 'random name', 'random color', 'enemy_id_3' 'image url', dead? 0 is dead, 1 is alive]
 
         //our image is in position 5
         cardStats.push(getRandomImageURL());
 
+        cardStats.push(1);
         //console.log("card stats: " + cardStats);
         // [3, 6, 'aoughalkh'];
         if (i < cardsBankLength/2 ) {
@@ -99,7 +108,54 @@ function getRandomName() {
     return ret;
 }
 function cardBattle() {
+    for (var i = 0; i < 3; i++) {
+        if (playerCards[i][6] == 1 && enemyCards[i][6] == 1) {
+            battle(i);
+        }
+        else if (playerCards[i][6] == 0 && enemyCards[i][6] == 1) {
+            playerHealth -= enemyCards[i][0];
+        }
+        else if (playerCards[i][6] == 1 && enemyCards[i][6] == 0) {
+            enemyHealth -= playerCards[i][0];
+        }
+        //what is the third possibility?  How should we handle it?
+
+    }
+    updateDisp();
+}
+
+function cardDeath(playerEnemy, i) {
+    cards = document.getElementsByClassName('card');
+    if (playerEnemy == 'player') {
+        cards[i+3].style.backgroundColor = "black";
+        console.log("player card " + playerCards[i][2] + " is dead");
+    }
+
+    if (playerEnemy == 'enemy') {
+        cards[i].style.backgroundColor = "black";
+        console.log("enemy card " + enemyCards[i][2] + " is dead");
+
+    }
+}
+function battle(i) {
+    //player health loses whatever the enemies attack is:
+
+    playerCards[i][1] -= enemyCards[i][0];
+    //enemy health same deal:
+    enemyCards[i][1] -= playerCards[i][0];
+    initializeCards();
+    if (playerCards[i][1] <= 0) {
+        playerCards[i][6] = 0; //card is dead
+        cardDeath('player', i);
+    }
+    if (enemyCards[i][1] <= 0) {
+        enemyCards[i][6] = 0; //card is dead
+        cardDeath('enemy', i);
+    }
     
+    //console.log("player cards: ", playerCards[i]);
+    //console.log("enemy cards: ", enemyCards[i]);
+
 }
 
 function getRandomColor() {
@@ -110,14 +166,14 @@ function getRandomColor() {
     }
     return ret;
 }
-function initializeStartingCards(){
+function initializeCards(){
     //console.log("Calling");
     //Get a list of all the cards damage in play in the game:
-    var cardsDamage = document.getElementsByClassName('card-attack');
-    var cardsDefense= document.getElementsByClassName('card-defense');
-    var cardsName = document.getElementsByClassName('card-name');
-    var cards = document.getElementsByClassName('card');
-    var cardImage = document.getElementsByTagName('img');
+    cardsDamage = document.getElementsByClassName('card-attack');
+    cardsDefense= document.getElementsByClassName('card-defense');
+    cardsName = document.getElementsByClassName('card-name');
+    cards = document.getElementsByClassName('card');
+    cardImage = document.getElementsByTagName('img');
     //enemy card initializiations:
     for (var i = 0; i < 6; i++) {
         //enemy cards: 
@@ -132,20 +188,20 @@ function initializeStartingCards(){
 
         cards[i].style.backgroundColor = enemyCards[i][3];
         
-        cards[i].id = "enemy_id_" + enemyCards[i][4];
+        cards[i].id = "player_id_" + enemyCards[i][4];
         cardImage[i].src = enemyCards[i][5];
         }
         //player cards:
         else {
         //set the attack of the enemy card damage by accessing the list at i
-        cardsDamage[i].innerHTML = "Attack: " + playerCards[i][0];
+        cardsDamage[i].innerHTML = "Attack: " + playerCards[i-3][0];
         //set the defense
-        cardsDefense[i].innerHTML = "Defense: " + playerCards[i][1];
+        cardsDefense[i].innerHTML = "Defense: " +  playerCards[i-3][1];
         //set the name
-        cardsName[i].innerHTML = playerCards[i][2];
+        cardsName[i].innerHTML = playerCards[i-3][2];
 
-        cards[i].style.backgroundColor = playerCards[i][3];
-        cardImage[i].src = playerCards[i][5];
+        cards[i].style.backgroundColor = playerCards[i-3][3];
+        cardImage[i].src = playerCards[i-3][5];
 
         }
 
@@ -162,8 +218,7 @@ function getRandomImageURL() {
 
 function enemyTurn() {
     if(!gameOver()) {
-    playerHealth -= 10;
-    updateDisp();
+        //enemy Turn code here:
     gameOver();
     }
 }
@@ -174,15 +229,18 @@ function startGame() {
     setUpCardBanks();
 
     //set up cards in play
-    initializeStartingCards();
+    initializeCards();
     
-    playerTurnButton.disabled = false;
-    enemyTurnButton.disabled = false;  
+    enableAllButtons();
     upgradeButton.removeEventListener('click', startGame);
     upgradeButton.innerHTML = 'Upgrade';
     upgradeButton.addEventListener('click', upgrade);
 }
-
+function enableAllButtons() {
+    playerTurnButton.disabled = false;
+    doneButton.disabled = false;  
+    upgrade.disabled = false;
+}
 function upgrade() {
     console.log("Upgrade");
 }
